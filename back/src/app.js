0f88207658632ff75990109afbd2e5c5b72975fa
JAASM1 <alexdroide466@gmail.com>
2025-06-21 17:49:49 -0500

    feat: estructura iniciar del backend con rutas, controladores, validaciones y modelos

---
+++ back/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import router from "./routes/contactRoutes.js";
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+  })
+);
+
+
+app.use("/api", router);
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: "Ruta no encontrada" });
+});
+
+
+export default app;
---
+++ back/src/routes/contactRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import rateLimit from "express-rate-limit";
+import {
+  validateContact,
+  createContact,
+  getContacts,
+  markAsRead,
+  deleteContact,
+} from "../controllers/contactControllers.js";
+
+const router = express.Router();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //15 mins
+  max: 5,
+  message: { error: "Ups. Demasiadas solicitudes. Intenta en 15 minutos." },
+});
+
+router.post("/contact", limiter, validateContact, createContact);
+router.get("/contacts", getContacts);
+router.patch("/contact/:id/read", markAsRead);
+router.delete("/contact/:id/delete", deleteContact);
+
+export default router;
